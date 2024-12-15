cb1536d4a6cca66bc1a13a747a2dad40be812d2e
Vince <vinceboelsen@outlook.com>
2024-12-15 16:19:20 +0100

    Reclame tekst 3

--- tijdelijk.py
+++ tijdelijk.py
@@ -9,4 +9,6 @@ aanbieding = (float(prijzen["aardbei"]) * 0.8)
 reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter – slechts € {aanbieding}")
 reclame_tekst2 = reclame_tekst[:63]
 
-print (reclame_tekst2)
+reclame_tekst3 = reclame_tekst2.upper()
+
+print (reclame_tekst3)
