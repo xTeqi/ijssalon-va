5c17146a4a8747b2ec8c1862ee6201807b300109
Vince <vinceboelsen@outlook.com>
2024-12-22 17:27:36 +0100

    Functie 1 - aangepast

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,3 +1,6 @@
-def mijn_functie_1(bedrag):
-    return pow(bedrag,2)
-print(mijn_functie_1(12))
+basisgetal = 4
+
+def mijn_functie_1():
+    global basisgetal
+    return pow(basisgetal,2)
+print(mijn_functie_1())
