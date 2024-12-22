694de517411b409a5d958530db95ea8e658b153b
Vince <vinceboelsen@outlook.com>
2024-12-22 18:14:12 +0100

    Functie 2 compleet

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,6 +1,18 @@
-basisgetal = 4
+basisgetal = 12
+modifier = 3
 
 def mijn_functie_1():
     global basisgetal
     return pow(basisgetal,2)
-print(mijn_functie_1())
+print (f"Functie 1: {mijn_functie_1()}")
+
+def mijn_functie_2():
+    global basisgetal
+    global modifier
+    return [
+        basisgetal + modifier, 
+        basisgetal - modifier, 
+        basisgetal * modifier, 
+        basisgetal // modifier
+    ]
+print (f"Functie 2: {mijn_functie_2()}")
