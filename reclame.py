047b5e4f0925fa32ebf9fe6809d2542bfdb1b068
Vince <vinceboelsen@outlook.com>
2024-12-24 15:19:49 +0100

    Inkomsten per week V2

--- reclame.py
+++ reclame.py
@@ -1,35 +1,12 @@
+## Aanbieding 1
 def aanbieding_1(smaak, prijs, korting):
     prijs_na_korting = prijs * (1 - korting)
     return f"Vandaag in de aanbieding: Emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} voor €{prijs_na_korting:.2f} euro."
 print (aanbieding_1("aardbei",4,0.1))
 
-def inkomsten_totaal(inkomsten):
-    return sum(inkomsten)
-weekinkomsten = [220,430,125,160,205,90,345]
-print (inkomsten_totaal(weekinkomsten))
-
-"""
-from algemene_functies import mijn_functie_2
-
-def inkomsten_totaal():
-    inkomsten = sum([220,430,125,160,205,90,345])
-    btw = inkomsten * 0.09
-    inkomsten_overzicht = f"Het totaal van alle inkomsten deze week is {inkomsten} euro, waarover {btw} euro btw betaald dient te worden."
-    return inkomsten_overzicht
-print(inkomsten_totaal())
-
-def laag_en_hoog():
-    mijn_lijst = [220,430,125,160,205,90,345]
-    return [
-        min(mijn_lijst),
-        max(mijn_lijst)
-    ]
-print (laag_en_hoog())
-
-def gemiddelde():
-    mijn_lijst = [220,430,125,160,205,90,345]
-    gemiddeld_bedrag = sum(mijn_lijst) / len(mijn_lijst)
-    gemiddeld_bedrag_format = f"{gemiddeld_bedrag:.2f}".replace('.' , ',')
-    return f"De gemiddelde inkomsten deze week zijn {gemiddeld_bedrag_format} euro."
-print (gemiddelde())
-"""
+#Inkomstentotaal per week inclusief btw
+def inkomsten_totaal(inkomsten, btw):
+    inkomsten_totaal_uitvoer = sum(inkomsten)
+    btw_bedrag = sum(inkomsten) * (1 * btw)
+    return f"Het totaal van alle inkomsten van deze week is {inkomsten_totaal_uitvoer:.2f} euro, waarover {btw_bedrag:.2f} euro btw betaald moet worden."
+print(inkomsten_totaal([220, 430, 125, 160, 205, 90, 345], 0.09))
