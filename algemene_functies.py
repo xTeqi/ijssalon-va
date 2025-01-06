78e169acb7e7e13a367848fcb1a306c3b54812b6
Vince <vinceboelsen@outlook.com>
2025-01-06 15:09:31 +0100

    Mijn Functie 2, list

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,3 +1,12 @@
 def mijn_functie_1(a):
     return a * a
 print(mijn_functie_1(12))
+
+def mijn_functie_2(a,b):
+    mijn_functie_2_list = []
+    mijn_functie_2_list.append(a + b)
+    mijn_functie_2_list.append(a - b)
+    mijn_functie_2_list.append(a * b)
+    mijn_functie_2_list.append(a // b)
+    return mijn_functie_2_list
+print(mijn_functie_2(12,2))
