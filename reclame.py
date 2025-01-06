47862c10f8d9b43965f9842d9e6542bcc93f98e5
Vince <vinceboelsen@outlook.com>
2025-01-06 15:16:08 +0100

    Aanbieding 1

--- reclame.py
+++ reclame.py
@@ -1,46 +1,8 @@
 ## Imports
 from algemene_functies import mijn_functie_2
 
-## Inkomsten per dag lijst
-inkomsten_per_dag = [220, 430, 125, 160, 205, 90, 345]
-
 ## Aanbieding 1
 def aanbieding_1(smaak, prijs, korting):
     prijs_na_korting = prijs * (1 - korting)
     return f"Vandaag in de aanbieding: Emmertje ijs (1 liter) in de smaak {smaak}, van €{prijs:.2f} voor €{prijs_na_korting:.2f} euro."
-
-# Inkomstentotaal per week inclusief btw
-def inkomsten_totaal(inkomsten, btw):
-    inkomsten_totaal_uitvoer = sum(inkomsten)
-    btw_bedrag = sum(inkomsten) * (1 * btw)
-    return f"Het totaal van alle inkomsten van deze week is {inkomsten_totaal_uitvoer:.2f} euro, waarover {btw_bedrag:.2f} euro btw betaald moet worden."
-
-## Laag en hoog
-def laag_en_hoog(mijn_lijst):
-    laag = min(mijn_lijst)
-    hoog = max(mijn_lijst)
-    return [hoog,laag]
-
-## Gemiddelde
-def gemiddelde(mijn_lijst):
-    gemiddelde_bedrag = sum(mijn_lijst) / len(mijn_lijst)
-    return f"De gemiddelde inkomsten deze week zijn {gemiddelde_bedrag:.2f} euro."
-
-## Meervoudig
-def meervoudig(invoer_lijst):
-    return laag_en_hoog(invoer_lijst)
-
-## Combinatie
-def combinatie(invoer_lijst_2):
-    korte_lijst = meervoudig(invoer_lijst_2)
-    uitvoer = mijn_functie_2()
-    return uitvoer
-
-## Digitale inkt
-print(
-    aanbieding_1("aardbei",4,0.1), "\n",
-    inkomsten_totaal(inkomsten_per_dag, 0.09), "\n",
-    laag_en_hoog(inkomsten_per_dag), "\n",
-    meervoudig([10,5,3,2,1,2,9]), "\n",
-    combinatie([""])
-    )
+print(aanbieding_1("aardbei",4,0.1))
