35af52a4a62c27b91a046e4787eb37f706740c06
Vince <vinceboelsen@outlook.com>
2025-01-13 16:10:35 +0100

    Fooi PP

--- helper.py
+++ helper.py
@@ -6,3 +6,12 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+    
+def fooi_pp(bedrag,personen):
+    bedrag_pp = bedrag / personen
+    return (f"Het bedrag per persoon is {bedrag_pp:.2f} euro.")
+
+b = int(input("Welk bedrag zit er in de fooienpot? "))
+p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
+
+print(fooi_pp(b,p))
